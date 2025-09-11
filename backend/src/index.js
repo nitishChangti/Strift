88062c650484f4e0a79de730c2da9367209bdc7a
Nitish Changti <nitishchangtiofficial@gmail.com>
2025-09-11 22:22:36 +0530

    some update in backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,27 +3,37 @@ import { DB_NAME } from './constants.js';
 import connectionDB from './db/index.js';
 import { app } from './app.js';
 
-// import serverless from 'serverless-http';
-
-// export const handler = serverless(app);
+import serverless from 'serverless-http';
 
 dotenv.config({
     path: '../.env'
 })
+await connectionDB().catch((error) => {
+    console.log("MONGODB db connection failed !!!", error);
+    process.exit(1);
+});
 
-connectionDB()
-    .then(() => {
-        app.on('error', (error) => {
-            console.log("err", error)
-            throw error
-        })
+// ❌ Remove app.listen()
+// ✅ Export a handler for Vercel
+export const handler = serverless(app);
 
-        console.log(`server is running on port ${process.env.PORT}`)
-        app.listen(process.env.PORT || 8080, () => {
-            console.log(`server is running on port ${process.env.PORT}`)
-        })
+// dotenv.config({
+//     path: '../.env'
+// })
 
-    })
-    .catch((error) => {
-        console.log('MONGODB db connection failed !!!', error)
-    })
+// connectionDB()
+//     .then(() => {
+//         app.on('error', (error) => {
+//             console.log("err", error)
+//             throw error
+//         })
+
+//         console.log(`server is running on port ${process.env.PORT}`)
+//         app.listen(process.env.PORT || 8080, () => {
+//             console.log(`server is running on port ${process.env.PORT}`)
+//         })
+
+//     })
+//     .catch((error) => {
+//         console.log('MONGODB db connection failed !!!', error)
+//     })
